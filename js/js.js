eb22fafaa02b1857c02efcdb85b83a08f1513e2b
Taron Julhakyan <t.julhakyanrr@mail.ru>
2020-11-20 12:47:11 +0100

    change classname

--- js/js.js
+++ js/js.js
@@ -1,11 +1,12 @@
 var element = document.getElementById("ho");
 
 element.addEventListener('click', function (event) {
-    element.innerHTML= "<div class='hallo'>Paragraph changed!</div>";
+    element.innerHTML= "<div>Paragraph changed!</div>";
+    element.id = "ape"
+    element.classList.add("hallo");
 });
 
 var div = document.getElementsByClassName('hallo')[0];
-
  div.addEventListener('click', function (event) {
      alert('Hooooi!');
  });
